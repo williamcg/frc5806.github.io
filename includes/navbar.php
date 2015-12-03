ffc3083f0e7584e3e37a6b2986f60c1f88a079df
joshuagruenstein <joshuagruenstein@gmail.com>
2015-12-03 11:46:38 -0500

    header and welcome

--- includes/navbar.php
+++ includes/navbar.php
@@ -4,10 +4,9 @@
 	</div>
 
 	<div id="nav" class="eleven columns"><ul>
-		<li><a href="#">About</a></li>
 		<li><a href="#">FRC</a></li>
 		<li><a href="#">FTC</a></li>
 		<li><a href="#">FLL</a></li>
-		<li><a href="#">Outreach</a></li>
+		<li><a href="#">News</a></li>
 	</ul></div>
 </div>
--- index.php
+++ index.php
@@ -14,15 +14,26 @@
 	<link rel="stylesheet" href="lib/skeleton.css" />
 
 	<link rel="stylesheet" href="style/general.css" />
+	<link rel="stylesheet" href="style/index.css" />
 </head>
 
 <body>
 
-	<div class="container">
-		<?php include 'includes/navbar.php' ?>
+<div class="container">
+	<?php include 'includes/navbar.php' ?>
+
+	<div id="header" class="row">
+		<img src="img/fullprogram.png">
 	</div>
 
-	<script src="script/index.js"></script>
+	<div id="welcome" class="row">
+		<h3>Basement Lions Robotics</h3>
+		<p>Horace Mann School's FIRST robotics teams.</p>
+	</div>
+</div>
+
+<script src="script/index.js"></script>
+
 </body>
 
 </html>
