2a3799f53ccab4aafd347a6bb6ddabf028be6488
jybster <justin_burton@horacemann.org>
2016-02-22 17:00:38 -0500

    navbar changes

--- js/general.js
+++ js/general.js
@@ -7,7 +7,8 @@ function navBar() {
 	$.get("includes/navbar.html", function(data) {
 		$("body").prepend(data);
 		
-		if(location.pathname.includes("about")) $("#aboutTab").addClass("active")
+		if (location.pathname.includes("index")) $("#indexTab").addClass("active")
+		else if(location.pathname.includes("about")) $("#aboutTab").addClass("active")
 		else if(location.pathname.includes("robots")) $("#robotsTab").addClass("active")
 		else if(location.pathname.includes("outreach")) $("#outreachTab").addClass("active")
 	});
